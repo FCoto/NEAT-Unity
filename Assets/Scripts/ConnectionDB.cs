189f26b44964e459c7e3b9889244d0ee90ac611d
Felix Coto <felixcg2017@gmail.com>
2019-09-14 13:50:11 +0200

    ConnectionDB clean

--- Assets/Scripts/ConnectionDB.cs
+++ Assets/Scripts/ConnectionDB.cs
@@ -6,26 +6,6 @@ public static class ConnectionDB
 
     static List<Connection> connectionList = new List<Connection>();
 
-    public static bool CheckIfConnectionExists(Connection conn)
-    {
-        foreach(Connection c in connectionList)
-        {
-            if (c.inputNode.id == conn.inputNode.id && c.outputNode.id == conn.outputNode.id)
-                return true;
-        }
-    
-        return false;
-    }
-
-    public static bool AddConnection(Connection conn)
-    {
-        if (!CheckIfConnectionExists(conn))
-        {
-            connectionList.Add(conn);
-            return true;
-        }
-        else return false;
-    }
 
     public static int GetCurrentInnovationNumber(Connection conn)
     {
